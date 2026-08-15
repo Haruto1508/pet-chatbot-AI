c999d0ae843457121ead90c18438cc4a820ef360
Haruto1508 <vinhaugust1508@gmail.com>
2026-08-15 22:27:59 +0700

    fix ts

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import getApp from '../server.ts';
+import getApp from '../server';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const app = await getApp();
