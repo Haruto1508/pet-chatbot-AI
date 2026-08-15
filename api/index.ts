503f5d8da3d5c71aadd9d4e263b248e6424d5383
Haruto1508 <vinhaugust1508@gmail.com>
2026-08-15 16:42:23 +0700

    fix vercel module path

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import getApp from '../dist/server.cjs';
+import getApp from '../server.ts';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const app = await getApp();
